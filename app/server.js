a50f8beb00770bac45179b248e25fec5f8744295
sameerbhadouria <sameer.bhadouria+github@gmail.com>
2012-08-20 22:39:10 -0700

    Cleaned up server and index.html file. Added email text box

--- app/server.js
+++ app/server.js
@@ -12,34 +12,30 @@ app.get('/', function(req, resp){
 	resp.sendfile('app/index.html');
 });
 
-app.get('/stackoverflow', function(req, resp) {
-	request({uri: 'http://stackoverflow.com/users/569413/dsilver829'}, function(err, response, body){
+app.get('/search', function(req, resp) {
+	var result = {};
 
-		var self = this;
-		self.items = new Array();
+	email = req.email;
 
-		if (err && response.statusCode !== 200) {
-		 console.log('Request error');
-		}
+	scrapeDataFromPage('http://stackoverflow.com/users/569413/' + email, 
+		'span.reputation-score', function(scrapedData) {
+			result.stackoverflowScore = scrapedData;
 
-		jsdom.env({
-			html: body,
-			scripts: ['http://code.jquery.com/jquery-latest.js']
-			}, 
-			function (err, window) {
-				var $ = window.jQuery;
+			scrapeDataFromPage('http://github.com/' + email, 
+				'.stats li strong', function(scrapedData) {
+					result.githubFollowers = scrapedData;
 
-				console.log($('span.reputation-score').first().text());
-				// resp.write('David Silver');
-				// respn.write('Reputation');
-				resp.end($('span.reputation-score').first().text());
-		});
+					scrapeDataFromPage('http://www.linkedin.com/pub/david-silver/0/607/99b', 
+						'.overview-connections strong', function(scrapedData) {
+							result.linkedinConnections = scrapedData;
+							resp.end(JSON.stringify(result));
+					});
+			});
 	});
 });
 
-app.get('/github', function(req, resp){
-	request({uri: 'http://github.com/dsilver829'}, function(err, response, body){
-
+scrapeDataFromPage = function(pageLink, dataToBeScraped, callback) {
+	request({uri: pageLink}, function(err, response, body) {
 		var self = this;
 		self.items = new Array();
 
@@ -53,39 +49,12 @@ app.get('/github', function(req, resp){
 			}, 
 			function (err, window) {
 				var $ = window.jQuery;
-
-				console.log($('.stats li strong').first().text());
-				// resp.write('David Silver');
-				// respn.write('Reputation');
-				resp.end($('.stats li strong').first().text());
+				var result = $(dataToBeScraped).first().text();
+				console.log(result);
+				callback(result);
 		});
 	});
-});
-
-app.get('/linkedin', function(req, resp){
-	request({uri: 'http://www.linkedin.com/pub/david-silver/0/607/99b'}, function(err, response, body){
-
-		var self = this;
-		self.items = new Array();
-
-		if (err && response.statusCode !== 200) {
-		 console.log('Request error');
-		}
-
-		jsdom.env({
-			html: body,
-			scripts: ['http://code.jquery.com/jquery-latest.js']
-			}, 
-			function (err, window) {
-				var $ = window.jQuery;
-
-				console.log($('.overview-connections strong').first().text());
-				// resp.write('David Silver');
-				// respn.write('Reputation');
-				resp.end($('.overview-connections strong').first().text());
-		});
-	});
-})
+}
 
 var port = process.env.PORT || 3000;
 
