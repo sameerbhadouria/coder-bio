7dfb707e9766a3573c6cf7177b08b35702a4849b
sameerbhadouria <sameer.bhadouria+github@gmail.com>
2012-08-20 23:00:18 -0700

    mocha test

--- test/server-test.js
+++ test/server-test.js
@@ -5,38 +5,41 @@ var assert = require('assert');
 // Here we load our server.js as a module
 var server = require('../app/server');
 
-describe('GET /', function() {
-	it('responds with default data', function(done) {
-		request(server)
-			.get('/')
-			.end(function(err, res) {
-				// Make sure there was no error
-				assert.equal(err, null);
+describe('Server', function() {
+	describe('GET /', function() {
+		it('responds with the index.html page', function(done) {
+			request(server)
+				.get('/')
+				.end(function(err, res) {
+					// Make sure there was no error
+					assert.equal(err, null);
 
-				var body = res.body;
-				//assert.equal(body.data, 'default data');
-				assert.equal(res.statusCode, 200);
+					var body = res.body;
+					//assert.equal(body.data, 'default data');
+					assert.equal(res.statusCode, 200);
 
-				// Finish asynchronous test
-				done();
-			});
+					// Finish asynchronous test
+					done();
+				});
+		});
+	});
+
+	describe('GET /search', function() {
+		it('responds with results data', function(done) {
+			request(server)
+				.get('/search')
+				.end(function(err, res) {
+					// Make sure there was no error
+					assert.equal(err, null);
+
+					var body = res.body;
+					//assert.equal(body.data, 'default data');
+					assert.equal(res.statusCode, 200);
+
+					// Finish asynchronous test
+					done();
+				});
+		});
 	});
 });
 
-describe('GET /search', function() {
-	it('responds with results data', function(done) {
-		request(server)
-			.get('/search')
-			.end(function(err, res) {
-				// Make sure there was no error
-				assert.equal(err, null);
-
-				var body = res.body;
-				//assert.equal(body.data, 'default data');
-				assert.equal(res.statusCode, 200);
-
-				// Finish asynchronous test
-				done();
-			});
-	});
-});
